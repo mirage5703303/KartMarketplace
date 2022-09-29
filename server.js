b665bd4a0d4cb358a21d2c988e9d1250c543250c
CrazyDev333 <104314209+CrazyDev333@users.noreply.github.com>
2022-09-29 20:45:36 +0000

    sixth commit

--- server.js
+++ server.js
@@ -56,7 +56,6 @@ app.get('*', (req, res) => {
 //   });
 // }
 
-// const port = process.env.PORT || 5000;
-const port = 80;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
